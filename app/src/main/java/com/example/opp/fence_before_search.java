bb912fd0bf0b817fdc61ebca6a4adcb905a9c12e
chenda <972347227@qq.com>
2019-01-27 15:36:38 +0800

    完成所有标题返回事件+fence after search 的搜索值传递+timepicker还未调试好

--- app/src/main/java/com/example/opp/fence_before_search.java
+++ app/src/main/java/com/example/opp/fence_before_search.java
@@ -1,9 +1,13 @@
 package com.example.opp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class fence_before_search extends AppCompatActivity {
 
@@ -11,13 +15,24 @@ public class fence_before_search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fence_before_search);
-        Button button = findViewById(R.id.button);
+        final EditText tv_search_vaule = findViewById(R.id.search_position);
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fence_before_search.this.onBackPressed();  //返回上一级页面；
             }
         });
+        Button button_search = (Button) findViewById(R.id.button_search);
+        button_search.setOnClickListener(new View.OnClickListener() {    //搜索后；
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(fence_before_search.this,fence_after_search.class);
+                intent.putExtra("search_value",tv_search_vaule.getText().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
