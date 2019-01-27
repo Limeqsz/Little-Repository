1d985de23e9f98837829933e48b5a0efe2ab51d6
chenda <972347227@qq.com>
2019-01-27 14:23:43 +0800

    fence before search 的部分更改

--- app/src/main/java/com/example/opp/fence_before_search.java
+++ app/src/main/java/com/example/opp/fence_before_search.java
@@ -2,6 +2,8 @@ package com.example.opp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class fence_before_search extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class fence_before_search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fence_before_search);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fence_before_search.this.onBackPressed();  //返回上一级页面；
+            }
+        });
     }
+
 }
