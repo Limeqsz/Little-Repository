d6e6ad508779ad1cdb7c609c96b6ad3a510275ff
Alderson <15267266525@163.com>
2019-01-27 13:03:26 +0800

    给我推一下啊

--- app/src/main/java/com/example/opp/MainActivity.java
+++ app/src/main/java/com/example/opp/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.opp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
