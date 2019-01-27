60da4c3079e26473bce1125443c41af917a190aa
chenda <972347227@qq.com>
2019-01-27 18:23:18 +0800

    变量定义置顶

--- app/src/main/java/com/example/opp/call_main.java
+++ app/src/main/java/com/example/opp/call_main.java
@@ -6,13 +6,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class call_main extends AppCompatActivity {
-
+    private Button button_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_record_main);
-        Button button = (Button) findViewById(R.id.title_back);
-        button.setOnClickListener(new View.OnClickListener() {
+        button_back = (Button) findViewById(R.id.title_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                     call_main.this.onBackPressed();  //返回上一级页面；
--- app/src/main/java/com/example/opp/fence_after_search.java
+++ app/src/main/java/com/example/opp/fence_after_search.java
@@ -10,23 +10,27 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class fence_after_search extends AppCompatActivity {
+    private Button button_back;
+    private  TextView tv_search_value;
+    private  TimePicker startTimepicker;
+    private TimePicker endTimepicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fence_after_search);
-        Button button = findViewById(R.id.button);
-        TextView tv_search_value = findViewById(R.id.search_position);
+        button_back = findViewById(R.id.button);
+        tv_search_value = findViewById(R.id.search_position);
         Intent intent = getIntent();
         String data = intent.getStringExtra("search_value");   //传递搜索结果；
         tv_search_value.setText(data);
-        button.setOnClickListener(new View.OnClickListener() {
+        button_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fence_after_search.this.onBackPressed();  //返回上一级页面；
             }
         });
         //开始时间选择……………………………………………………………………………………………………
-        TimePicker startTimepicker = (TimePicker)findViewById(R.id.start_date);
+        startTimepicker = (TimePicker)findViewById(R.id.start_date);
         startTimepicker.setDescendantFocusability(TimePicker.FOCUS_BLOCK_DESCENDANTS);  //设置点击事件不弹键盘
         startTimepicker.setIs24HourView(true);   //设置时间显示为24小时
         startTimepicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {  //获取当前选择的时间
@@ -35,7 +39,7 @@ public class fence_after_search extends AppCompatActivity {
             }
         });
 //        结束时间选择………………………………………………………………………………………………………
-        TimePicker endTimepicker = (TimePicker)findViewById(R.id.end_date);
+        endTimepicker = (TimePicker)findViewById(R.id.end_date);
         endTimepicker.setDescendantFocusability(TimePicker.FOCUS_BLOCK_DESCENDANTS);  //设置点击事件不弹键盘
         endTimepicker.setIs24HourView(true);   //设置时间显示为24小时
         endTimepicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {  //获取当前选择的时间
