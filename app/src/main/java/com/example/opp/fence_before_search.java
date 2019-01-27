c6131ab1887b9952ae02fb54b8310cb0fd5f6c93
wenjie” <xxx185829797@qq.com>
2019-01-27 12:45:55 +0800

    1、合并了部分布局文件（直接把标题栏加在原布局文件里） 2、给首页各Button加了对应事件并创建了首页各Button的活动（首页基本跳转全部实现发现首页上方两个Button没有对应布局文件，加号和用户Button） 3、不知何原因，Recyclerview会导致程序闪退，聊天和通话记录部分全部改为Listview 4、电子围栏标题栏问题待解决（在手机上运行，不会置顶）

--- app/src/main/java/com/example/opp/fence_before_search.java
+++ app/src/main/java/com/example/opp/fence_before_search.java
@@ -1,12 +1,13 @@
 package com.example.opp;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class fence_before_search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fence_before_search);
     }
 }
