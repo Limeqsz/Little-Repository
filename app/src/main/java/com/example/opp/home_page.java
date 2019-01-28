e2af5d5bd3c04203929e19502eab6d1ed94de445
Alderson <15267266525@163.com>
2019-01-28 19:37:51 +0800

    修改菜单栏，滑动菜单等，v0.1，带修改小问题无法全局滑动

--- app/src/main/java/com/example/opp/home_page.java
+++ app/src/main/java/com/example/opp/home_page.java
@@ -1,32 +1,43 @@
 package com.example.opp;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public class home_page extends AppCompatActivity {
+
+    //滑动布局
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-        Button button_title_back = (Button) findViewById(R.id.title_back);
-        button_title_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(home_page.this,"这个页面还没写好",Toast.LENGTH_LONG ).show();
-            }
-        });
-        Button button_yonghu = (Button) findViewById(R.id.title_yonghu);
-        button_yonghu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(home_page.this,"这个页面还没写好",Toast.LENGTH_LONG ).show();
-            }
-        });
+//        Button button_title_back = (Button) findViewById(R.id.title_back);
+//        button_title_back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(home_page.this,"这个页面还没写好",Toast.LENGTH_LONG ).show();
+//            }
+//        });
+//        Button button_yonghu = (Button) findViewById(R.id.title_yonghu);
+//        button_yonghu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(home_page.this,"这个页面还没写好",Toast.LENGTH_LONG ).show();
+//            }
+//        });
         Button button_fence = (Button) findViewById(R.id.fence);
         button_fence.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +79,37 @@ public class home_page extends AppCompatActivity {
             }
         });
 
+        //实例化标题栏
+        Toolbar toolbar = findViewById(R.id.home_page_title);
+        setSupportActionBar(toolbar);
+        //实例化滑动布局
+        mDrawerLayout=findViewById(R.id.home_drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ice_add_childern);
+        }
+    }
+
+    //title中的menu点击逻辑
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    //设置toolbar中menu的点击事件
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_setting:
+                mDrawerLayout.openDrawer(GravityCompat.END);
+                break;
+            default:
+        }
+        return true;
     }
 }
